f1f5f9bbac206b4fd85eab54ba20530d1bb03808
Gimleux <theo-2008@t-online.de>
2020-08-06 17:35:34 +0200

    Uebungen 08_Aufgaben zu Klassen - 2 Lotto

--- src/de/dhbwka/java/exercise/classes/Lotto.java
+++ src/de/dhbwka/java/exercise/classes/Lotto.java
@@ -3,6 +3,7 @@ package de.dhbwka.java.exercise.classes;
 import de.dhbwka.java.utilities.Input.SameLineInput;
 import de.dhbwka.java.utilities.console.Console;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lotto {
@@ -12,66 +13,119 @@ public class Lotto {
     private short[] drawnNumbers;
 
     public Lotto() {
-        this((short)6,(short)49);
+        this((short) 6, (short) 49);
     }
 
     public Lotto(short numberToDraw, short numberOfBalls) {
+        if (numberToDraw > numberOfBalls) {
+            Console.printlnColoredText("Anzahl der zu ziehenden Kugeln darf nicht kleiner der Anzahl aller Kugeln sein.", "red");
+            Console.printlnColoredText("Anzahl aller Kugeln -> Anzahl der zu ziehenden Kugeln +1", "yellow");
+            numberOfBalls = (short) (numberToDraw + 1);
+        }
         this.numberToDraw = numberToDraw;
         this.numberOfBalls = numberOfBalls;
+        this.tip = new short[numberToDraw];
+        this.drawnNumbers = new short[numberToDraw];
     }
 
-    public void draw() {
-        for (int i = 0; i<numberToDraw; ++i){
-            drawnNumbers[i] = (short) (Math.random()*numberOfBalls);
+    //TODO Hilfsklasse Array durchsuchen (mit und ohne sortieren) schreiben
+
+    public void ziehen() {
+        for (int i = 0; i < numberToDraw; ++i) {
+            short random;
+            do {
+                random = (short) ((Math.random() * numberOfBalls) + 1);
+            } while (arrayContainsNumber(drawnNumbers, random));
+            this.drawnNumbers[i] = random;
         }
+        Arrays.sort(drawnNumbers);
+    }
+
+    private boolean arrayContainsNumber(short[] array, short number) {
+        for (short value : array) {
+            if (value == number) return true;
+        }
+        return false;
     }
 
     public int richtige() {
-        short i = 0;
         short drawn = 0;
-        while(this.tip[i]!=0) {
-            i++;
-            drawn++;
+        while (drawn < numberToDraw && this.tip[drawn] != 0) {
+            ++drawn;
         }
         short right = 0;
-        if (drawn!=numberToDraw){
+        if (drawn != numberToDraw) {
             Console.printlnColoredText("Fehler: Nicht alle Tipps abgegeben!", "red");
         } else {
-            for (short tip : this.tip){
-                for (short number : this.drawnNumbers){
-                    if(tip==number) ++right;
+            for (short tip : this.tip) {
+                for (short number : this.drawnNumbers) {
+                    if (tip == number) ++right;
                 }
             }
         }
         return right;
     }
 
-    public void setTip(Scanner scan){
-        short i = 0;
-        while(this.tip[i]!=0) {
-            i++;
+    public void setAllTips(Scanner scan) {
+        while (this.tip[numberToDraw - 1] == 0) {
+            setTip(scan);
         }
-        short tip = (short) SameLineInput.getIntValueOfDescription(scan, "Tipp für die "+ (i+1) + ". Zahl");
-        if (tip <=numberOfBalls && tip>0) {
-            this.tip[i] = tip;
-        }
-                else Console.printlnColoredText("Fehler: Eingabe nicht im Bereich [1,"+numberOfBalls+"]", "red");
+        Arrays.sort(this.tip);
     }
 
-    private void setTip(short tip){
+    private void setTip(Scanner scan) {
         short i = 0;
-        while(this.tip[i]!=0) {
+        while (this.tip[i] != 0) {
             i++;
         }
-        if (tip <=numberOfBalls && tip>0)this.tip[i] = tip;
+        short tip = (short) SameLineInput.getIntValueOfDescription(scan, "Tipp für die " + (i + 1) + ". Zahl");
+        if (tip <= numberOfBalls && tip > 0 && this.tip[i] == 0) {
+            this.tip[i] = tip;
+        } else Console.printlnColoredText("Fehler: Eingabe nicht im Bereich [1," + numberOfBalls + "]", "red");
+    }
+
+    public void setTip(short tip) {
+        short i = 0;
+        while (this.tip[i] != 0) {
+            i++;
+        }
+        if (tip <= numberOfBalls && tip > 0 && this.tip[i] == 0) this.tip[i] = tip;
+        Arrays.sort(this.tip);
     }
 
     public void setTip(short[] tip) {
         this.tip = tip;
+        Arrays.sort(this.tip);
+    }
+
+    public short getNumberToDraw() {
+        return numberToDraw;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        boolean alreadyDrawn = (drawnNumbers[0] != 0);
+        boolean alreadyTiped = (tip[0] != 0);
+        String drawn = "Gezogene Zahlen:";
+        for (short number : drawnNumbers) drawn += (" " + number);
+        String tipsGiven = "Tipps: ";
+        for (short number : tip) if (number != 0) tipsGiven += (" " + number);
+        return (!alreadyTiped) ? "" : (tipsGiven) + ((!alreadyDrawn) ? "" : "\n" + drawn);
     }
 
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        Lotto germanLotto = new Lotto((short) 6, (short) 49);
+        germanLotto.setAllTips(scan);
+        germanLotto.ziehen();
+        System.out.println(germanLotto);
+        short richtige = (short)germanLotto.richtige();
+        String color;
+        if (richtige==germanLotto.getNumberToDraw()) color = "green bright";
+                else if (richtige>Math.nextUp(germanLotto.getNumberToDraw()/4.0)) color="yellow";
+                    else if (richtige>0)color="orange";
+                        else color = "red";
+        Console.printlnColoredText("Richtige: " + germanLotto.richtige(), color);
     }
 
 }
--- src/de/dhbwka/java/utilities/console/Console.java
+++ src/de/dhbwka/java/utilities/console/Console.java
@@ -5,25 +5,59 @@ public class Console {
     //Gibt ANSI-String mit gewuenschter Farbe zurueck um Ausgaben in der Konsole einzufaerben
     /* System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
         -> "\u001B" + Console.colorConsoleText + <Text> */
-    public static String colorConsoleText(String color){
-
+    public static String colorConsoleText(String color) {
+        //TODO: BG = Zahl+10
+        //TODO [VG;BGm
+        //TODO https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
         switch (color.toLowerCase()) {
-            case "black"    : return "\u001B[30m";
-            case "red"      : return "\u001B[31m";
-            case "green"    : return "\u001B[32m";
-            case "yellow"   : return "\u001B[33m";
-            case "blue"     : return "\u001B[34m";
-            case "purple"   : return "\u001B[35m";
-            case "cyan"     : return "\u001B[36m";
-            case "white"    : return "\u001B[37m";
-            case "reset"    :
-            case "normal"   :
-            case "default"  :
-            default         : return "\u001B[0m";
+            case "black":
+                return "\u001B[30m";
+            case "red":
+                return "\u001B[31m";
+            case "green":
+                return "\u001B[32m";
+            case "yellow":
+                return "\u001B[33m";
+            case "blue":
+                return "\u001B[34m";
+            case "magenta":
+            case "purple":
+                return "\u001B[35m";
+            case "cyan":
+                return "\u001B[36m";
+            case "white":
+                return "\u001B[37m";
+            case "orange":
+                return "\u001B[38;5;202m";
+            case "black bright":
+            case "gray":
+                return "\u001B[90m";
+            case "red bright":
+                return "\u001B[91m";
+            case "green bright":
+                return "\u001B[92m";
+            case "yellow bright":
+                return "\u001B[93m";
+            case "blue bright":
+                return "\u001B[94m";
+            case "magenta bright":
+            case "purple bright":
+                return "\u001B[95m";
+            case "cyan bright":
+                return "\u001B[96m";
+            case "white bright":
+                return "\u001B[97m";
+            case "orange bright":
+                return "\u001B[38;5;220m";
+            case "reset":
+            case "normal":
+            case "default":
+            default:
+                return "\u001B[0m";
         }
     }
 
-    public static void printlnColoredText(String text, String color){
+    public static void printlnColoredText(String text, String color) {
         System.out.println(colorConsoleText(color) + text + colorConsoleText("default"));
     }
 }
