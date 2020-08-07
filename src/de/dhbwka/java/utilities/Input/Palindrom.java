7e7e8910184c3a138456958a6eebcd84397dda0b
Gimleux <theo-2008@t-online.de>
2020-08-07 16:29:09 +0200

    Uebungen 10 -> 02 Palindrom

---
+++ src/de/dhbwka/java/utilities/Input/Palindrom.java
@@ -0,0 +1,40 @@
+package de.dhbwka.java.utilities.Input;
+
+import java.util.Scanner;
+
+public class Palindrom {
+    String word;
+
+    public void printTurnedAround(){
+        System.out.println("Umgekehrt: " + turnAround(word));
+    }
+
+    public void printIsPalindrom(){
+        System.out.println(word + " ist "+ (isPalindrom(word)?"":"k") + "ein Palindrom.");
+    }
+
+    private String turnAround(String text){
+        StringBuilder help = new StringBuilder();
+        return help.append(text).reverse().toString();
+    }
+
+    private boolean isPalindrom(String text){
+        return text.equalsIgnoreCase(turnAround(text));
+    }
+
+    public void getWord(){
+        Scanner scan = new Scanner(System.in);
+        setWord(OneLineInput.getStringValueOfDescription(scan, "Wort"));
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public static void main(String[] args) {
+        Palindrom palindrom = new Palindrom();
+        palindrom.getWord();
+        palindrom.printTurnedAround();
+        palindrom.printIsPalindrom();
+    }
+}
