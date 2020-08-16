d2d5f1cbd2df419fe68d2fa3daed62cc9c5479d5
Gimleux <theo-2008@t-online.de>
2020-08-16 08:38:49 +0200

    Changed utilities - ReadFileToString & added ReadFileToStringArray

--- src/de/dhbwka/java/utilities/files/read/wholeFiles/FileToStringArray.java
+++ src/de/dhbwka/java/utilities/files/read/wholeFiles/FileToStringArray.java
@@ -1,84 +1,85 @@
-package de.dhbwka.java.utilities.files.read;
+package de.dhbwka.java.utilities.files.read.wholeFiles;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class ReadWholeFiles {
+public class FileToStringArray {
     ///////////////////////////////////////////////////////////////////////////
     // get Text from File - With Error Description
     ///////////////////////////////////////////////////////////////////////////
+
     /**
      * Read a file by given pathname and return its content as String
      *
      * @param pathname path to file (with filename)
-     * @return String with file's content or written Error description
+     * @return StringArray with file's content or written Error description
      */
-    public static String getTextFromFile_WithErrorDescription(String pathname) {
-        File file = new File(pathname);
-        return getFileContentWithErrorDescription(file);
+    public static String[] getFileToStringArray_WithErrorDescription(String pathname) {
+        return getFileToStringArray_WithErrorDescription(new File(pathname));
     }
 
     /**
      * Read a file by given pathname and return its content as String
      *
      * @param file file that shall be read
-     * @return String with file's content or written Error description
+     * @return StringArray with file's content or written Error description
      */
-    public static String getTextFromFile_WithErrorDescription(File file) {
+    public static String[] getFileToStringArray_WithErrorDescription(File file) {
         return getFileContentWithErrorDescription(file);
     }
 
-    private static String getFileContentWithErrorDescription(File file) {
+    private static String[] getFileContentWithErrorDescription(File file) {
         if (!file.exists()) {
-            return "Fehler: Datei nicht vorhanden";
+            return new String[]{"Fehler: Datei nicht vorhanden"};
         }
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            StringBuilder stringBuilder = new StringBuilder();
+            ArrayList<String> arrayList = new ArrayList<>();
             while (bufferedReader.ready()) {
-                stringBuilder.append(bufferedReader.readLine()).append(System.lineSeparator());
+                arrayList.add(bufferedReader.readLine());
             }
-            return stringBuilder.toString();
+            return arrayList.toArray(new String[0]);
         } catch (IOException e) {
-            return "Fehler: Auf Datei konnte nicht zugegriffen werden";
+            return new String[]{"Fehler: Auf Datei konnte nicht zugegriffen werden"};
         }
     }
 
     ///////////////////////////////////////////////////////////////////////////
     // get Text from File - With Null if Error
     ///////////////////////////////////////////////////////////////////////////
+
     /**
      * Read a file by given pathname and return its content as String
      *
      * @param pathname path to file (with filename)
-     * @return String with file's content or null if Error occurs
+     * @return StringArray with file's content or null if Error occurs
      */
-    public static String getTextFromFile_NullIfError(String pathname) {
-        File file = new File(pathname);
-        return getFileContentWithNullIfError(file);
+    public static String[] getFileToString_NullIfError(String pathname) {
+        return getFileToString_NullIfError(new File(pathname));
     }
 
     /**
      * Read a file by given pathname and return its content as String
      *
      * @param file file that shall be read
-     * @return String with file's content or null if Error occurs
+     * @return StringArray with file's content or null if Error occurs
      */
-    public static String getTextFromFile_NullIfError(File file) {
+    public static String[] getFileToString_NullIfError(File file) {
         return getFileContentWithNullIfError(file);
     }
 
-    private static String getFileContentWithNullIfError(File file) {
+    private static String[] getFileContentWithNullIfError(File file) {
         if (!file.exists()) {
             return null;
         }
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            StringBuilder stringBuilder = new StringBuilder();
+            ArrayList<String> arrayList = new ArrayList<>();
             while (bufferedReader.ready()) {
-                stringBuilder.append(bufferedReader.readLine()).append(System.lineSeparator());
+                arrayList.add(bufferedReader.readLine());
             }
-            return stringBuilder.toString();
+            return arrayList.toArray(new String[0]);
         } catch (IOException e) {
             return null;
         }
@@ -87,21 +88,22 @@ public class ReadWholeFiles {
     ///////////////////////////////////////////////////////////////////////////
     // get Text from File - With Exception Message if Error
     ///////////////////////////////////////////////////////////////////////////
+
     /**
      * Read a file by given pathname and return its content as String
      *
      * @param pathname path to file (with filename)
-     * @return String with file's content or String with Exception message
+     * @return StringArray with file's content or String with Exception message
      */
-    public static String getTextFromFile_WithExceptionMessage(String pathname) { 
+    public static String[] getFileToString_WithExceptionMessage(String pathname) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(pathname)))) {
-            StringBuilder stringBuilder = new StringBuilder();
+            ArrayList<String> arrayList = new ArrayList<>();
             while (bufferedReader.ready()) {
-                stringBuilder.append(bufferedReader.readLine()).append(System.lineSeparator());
+                arrayList.add(bufferedReader.readLine());
             }
-            return stringBuilder.toString();
+            return arrayList.toArray(new String[0]);
         } catch (IOException e) {
-            return e.getMessage();
+            return new String[]{e.getMessage()};
         }
     }
 
@@ -109,17 +111,17 @@ public class ReadWholeFiles {
      * Read a file by given pathname and return its content as String
      *
      * @param file file that shall be read
-     * @return String with file's content or String with Exception message
+     * @return StringArray with file's content or String with Exception message
      */
-    public static String getTextFromFile_WithExceptionMessage(File file) {
+    public static String[] getFileToString_WithExceptionMessage(File file) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            StringBuilder stringBuilder = new StringBuilder();
+            ArrayList<String> arrayList = new ArrayList<>();
             while (bufferedReader.ready()) {
-                stringBuilder.append(bufferedReader.readLine()).append(System.lineSeparator());
+                arrayList.add(bufferedReader.readLine());
             }
-            return stringBuilder.toString();
+            return arrayList.toArray(new String[0]);
         } catch (IOException e) {
-            return e.getMessage();
+            return new String[]{e.getMessage()};
         }
     }
 }
