2c60819f3b611c36f1e339b5f082943dea790940
Gimleux <56401687+Gimleux@users.noreply.github.com>
2021-05-03 11:07:28 +0200

    Added Overwrite with Exception

--- src/de/dhbwka/java/utilities/files/write/overwrite/OverwriteFileWithString.java
+++ src/de/dhbwka/java/utilities/files/write/overwrite/OverwriteFileWithString.java
@@ -18,6 +18,7 @@ public class OverwriteFileWithString {
     public static String overwriteFileWithString_WithErrorDescription(String pathname, String text){
         return overwriteFileWithString_WithErrorDescription(new File(pathname), text);
     }
+
     /**
      * Append given text to given file
      * @param file to which the text shall be appended
@@ -28,12 +29,39 @@ public class OverwriteFileWithString {
         return overwriteContentWithErrorDescription(file, text);
     }
 
+    /**
+     * Overwrite file with given text
+     * @param pathname String with path to file in which the text shall be written
+     * @param text text that shall be written in file
+     * @return String with error message or null if no error
+     */
+    public static void overwriteFileWithString_WithException(String pathname, String text) throws IOException {
+        overwriteFileWithString_WithException(new File(pathname), text);
+    }
+
+    /**
+     * Append given text to given file
+     * @param file to which the text shall be appended
+     * @param text text that shall be written in file
+     * @return String with error message or null if no error
+     */
+    public static void overwriteFileWithString_WithException(File file, String text) throws IOException {
+        overwriteContentWithException(file, text);
+    }
+
     private static String overwriteContentWithErrorDescription(File file, String content){
         try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))){
             bufferedWriter.write(content);
+            bufferedWriter.close();
             return null;
         } catch (IOException e){
             return e.getMessage();
         }
+    }
+
+    private static void overwriteContentWithException(File file, String content) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+        bufferedWriter.write(content);
+        bufferedWriter.close();
     }
 }
